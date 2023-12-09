5775a41c62fd6995729254a0a93fe551bf12861e
svyatoslavkk <sv.krivonos1@gmail.com>
2023-12-09 20:41:14 +0300

    feat: add primary button and welcome text

---
+++ src/components/authButtons/AuthButtons.tsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from '../buttons/PrimaryButton';
+
+export default function AuthButtons() {
+  return (
+    <div>
+      <PrimaryButton>Sign In</PrimaryButton>
+      <PrimaryButton>Sign Up</PrimaryButton>
+    </div>
+  );
+}
---
+++ src/components/welcomeText/WelcomeText.tsx
@@ -0,0 +1,3 @@
+export default function WelcomeText() {
+  return <h1>Welcome to GraphiQL</h1>;
+}
