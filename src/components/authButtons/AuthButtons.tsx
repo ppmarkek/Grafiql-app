a454581df97a449339a0e2ac8e951abf08136660
svyatoslavkk <sv.krivonos1@gmail.com>
2023-12-09 20:59:52 +0300

    feat: add links to auth buttons

--- src/components/authButtons/AuthButtons.tsx
+++ src/components/authButtons/AuthButtons.tsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
 import { PrimaryButton } from '../buttons/PrimaryButton';
+import { ButtonsContainer } from './style';
 
 export default function AuthButtons() {
   return (
-    <div>
-      <PrimaryButton>Sign In</PrimaryButton>
-      <PrimaryButton>Sign Up</PrimaryButton>
-    </div>
+    <ButtonsContainer>
+      <Link to="/signin">
+        <PrimaryButton>Sign In</PrimaryButton>
+      </Link>
+      <Link to="/signup">
+        <PrimaryButton>Sign Up</PrimaryButton>
+      </Link>
+    </ButtonsContainer>
   );
 }
