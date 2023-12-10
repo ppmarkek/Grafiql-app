18f89d97f091f31b3ee2c633e5c1bebe6faa8bef
LisaW <LisaW@ya.ru>
2023-12-10 18:28:16 +0300

    GRAP-23 add firebase auth services & env

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as dotenv from 'dotenv';
 // import App from './App.tsx'
 import './index.css';
 import SignUp from './pages/SignUp/SignUp.js';
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
