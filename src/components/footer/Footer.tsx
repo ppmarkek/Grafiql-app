c43cb78557442b985f8c3c719b02cfa4ead25c86
svyatoslavkk <sv.krivonos1@gmail.com>
2023-12-12 13:49:38 +0300

    feat: add footer layout

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,27 @@
+import { FooterContainer } from '../../styles/commonStyles';
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <div>
+        <div>
+          <h3>Authors&apos; GitHub</h3>
+          <div>
+            <a href="" target="_blank"></a>
+            <a href="" target="_blank"></a>
+            <a href="" target="_blank"></a>
+          </div>
+        </div>
+        <div>
+          <h3>Year of Creation</h3>
+          <span>2023</span>
+        </div>
+        <div>
+          <a>
+            <img />
+          </a>
+        </div>
+      </div>
+    </FooterContainer>
+  );
+}
---
+++ src/styles/commonStyles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  background-color: #ddd;
+  padding: 1rem;
+`;
