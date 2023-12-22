91ce941ac4c22ff61ed198b575b73810f8c9d96e
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2023-12-22 18:00:10 +0200

    feat: add slider

--- src/components/templates/Documentation/Documentation.tsx
+++ src/components/templates/Documentation/Documentation.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { OpenButton, OpenGrid, Wrapper } from './style';
-import { Drawer } from '@mui/material';
+import { Drawer, Grid } from '@mui/material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Text from '../../atoms/Text/Text';
 
 const Documentation = () => {
   const [state, setState] = useState(false);
@@ -9,11 +10,23 @@ const Documentation = () => {
   return (
     <Wrapper>
       <OpenGrid>
-        <OpenButton>
+        <OpenButton
+          active={state}
+          onClick={() => setState(state ? false : true)}
+        >
           <ChevronRightIcon />
         </OpenButton>
       </OpenGrid>
-      <Drawer anchor={'left'} open={state}></Drawer>
+      <Drawer anchor={'left'} open={state}>
+        <Grid width={'400px'} padding={'20px'}>
+          <Grid display={'flex'} flexDirection={'column'} gap={'10px'}>
+            <Text variant={'H2'}>Docs</Text>
+            <Text variant={'REGULAR'}>
+              A GraphQL schema provides a root type for each kind of operation.
+            </Text>
+          </Grid>
+        </Grid>
+      </Drawer>
     </Wrapper>
   );
 };
--- src/components/templates/Documentation/style.ts
+++ src/components/templates/Documentation/style.ts
@@ -1,6 +1,10 @@
 import { Button, Grid } from '@mui/material';
 import { styled } from '@mui/system';
 
+type ButtonProps = {
+  active?: boolean;
+};
+
 export const Wrapper = styled(Grid)({
   height: '100%',
   width: '30px',
@@ -12,7 +16,7 @@ export const OpenGrid = styled(Grid)({
   borderRight: '1px solid #444',
 });
 
-export const OpenButton = styled(Button)({
+export const OpenButton = styled(Button)(({ active }: ButtonProps) => ({
   all: 'unset',
   display: 'flex',
   alignItems: 'center',
@@ -23,9 +27,15 @@ export const OpenButton = styled(Button)({
   height: '25px',
   cursor: 'pointer',
   position: 'absolute',
-  left: '17px',
-  top: '120px',
+  left: active ? '387px' : '17px',
+  transform: active ? 'rotate(0.5turn)' : 'none',
+  top: '220px',
+  zIndex: '1201',
+  transition: 'all 0.2s',
   span: {
     display: 'none',
   },
-});
+  ':hover': {
+    background: '#6B59CC',
+  },
+}));
