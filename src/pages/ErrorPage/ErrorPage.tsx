7d57468cbade00d69b50f97013862cdbb843ab49
svyatoslavkk <sv.krivonos1@gmail.com>
2023-12-24 22:24:45 +0300

    feat: navigate to main page to 404 btn

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,14 @@
 import { Space, Wrapper, Button, Text, Container, Digit } from './styles';
+import { useNavigate } from 'react-router-dom';
 import './errorPage.css';
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/');
+  };
+
   return (
     <Container>
       <div className="background-img">
@@ -14,7 +21,7 @@ export default function ErrorPage() {
             The page you are trying to search has been moved to another
             universe.
           </Text>
-          <Button>GET ME HOME</Button>
+          <Button onClick={handleButtonClick}>GET ME HOME</Button>
         </Wrapper>
       </div>
     </Container>
