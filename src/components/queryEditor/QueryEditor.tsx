adbe1367fa8a10c8828c0074bdf780f947967795
svyatoslavkk <sv.krivonos1@gmail.com>
2023-12-25 18:41:18 +0300

    fix: textarea window

--- src/components/queryEditor/QueryEditor.tsx
+++ src/components/queryEditor/QueryEditor.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import './QueryEditor.css';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 interface Props {
   onQuerySubmit: (query: string) => void;
 }
 
 export default function QueryEditor({ onQuerySubmit }: Props) {
+  const codeAreaRef = useRef<HTMLTextAreaElement | null>(null);
+  const lineNumbersRef = useRef<HTMLDivElement | null>(null);
+  const [lines, setLines] = useState(1);
   const [query, setQuery] = useState<string>('');
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -15,15 +20,62 @@ export default function QueryEditor({ onQuerySubmit }: Props) {
     onQuerySubmit(query);
   };
 
+  useEffect(() => {
+    const handleInput = () => {
+      updateLineNumbers();
+    };
+
+    const handleScroll = () => {
+      updateLineNumbers();
+    };
+
+    const codeArea = codeAreaRef.current;
+
+    if (codeArea) {
+      codeArea.addEventListener('input', handleInput);
+      codeArea.addEventListener('scroll', handleScroll);
+
+      return () => {
+        codeArea.removeEventListener('input', handleInput);
+        codeArea.removeEventListener('scroll', handleScroll);
+      };
+    }
+
+    return () => {};
+  }, []);
+
+  const updateLineNumbers = () => {
+    const codeArea = codeAreaRef.current;
+    if (codeArea) {
+      const codeLines = codeArea.value.split('\n').length;
+      setLines(codeLines);
+    }
+  };
+
   return (
-    <div>
+    <div className="code-editor">
+      <div
+        className="line-numbers"
+        ref={lineNumbersRef}
+        style={{ fontFamily: 'Consolas, monospace' }}
+      >
+        {Array.from({ length: lines }, (_, i) => i + 1).join('\n')}
+      </div>
       <textarea
-        value={query}
+        className="code-area"
+        defaultValue={query}
         onChange={handleQueryChange}
-        rows={20}
-        cols={40}
-      />
-      <button onClick={handleQuerySubmit}>Submit Query</button>
+        ref={codeAreaRef}
+        spellCheck="false"
+        onInput={updateLineNumbers}
+        onScroll={updateLineNumbers}
+        style={{ fontFamily: 'Consolas, monospace' }}
+      ></textarea>
+      <div className="buttons-section">
+        <button onClick={handleQuerySubmit}>
+          <PlayArrowIcon />
+        </button>
+      </div>
     </div>
   );
 }
