557d87a7d3f23f9c53f2e7027adf3bc48a75f259
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2024-01-03 17:43:16 +0200

    feat: fix all

--- src/components/atoms/InputEntryPoint/InputEntryPoint.tsx
+++ src/components/atoms/InputEntryPoint/InputEntryPoint.tsx
@@ -1,129 +1,23 @@
-import { Grid, Tabs } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import * as monaco from 'monaco-editor';
-import MonacoEditor from '@monaco-editor/react';
-import {
-  ButtonGrid,
-  ButtonStart,
-  GridTabs,
-  StyledButton,
-  StyledTab,
-  StyledTextField,
-} from './style';
-import { useState } from 'react';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Grid
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-      height={'70%'}
-    >
-      {value === index && <Grid height={'100%'}>{children}</Grid>}
-    </Grid>
-  );
-}
+import { ButtonStart, Wrapper, StyledInput } from './style';
+import { SetStateAction, useState } from 'react';
 
 const InputEntryPoint = () => {
-  const [value, setValue] = useState(0);
-  const [inputValue, setInputValue] = useState('');
+  const [value, setValue] = useState('');
 
-  const a11yProps = (index: number) => {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  };
-
-  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleEditorChange = (value: string | undefined) => {
-    setInputValue(value || '');
-  };
-
-  const options: monaco.editor.IStandaloneEditorConstructionOptions = {
-    selectOnLineNumbers: true,
-    roundedSelection: false,
-    readOnly: false,
-    cursorStyle: 'line',
-    glyphMargin: true,
-    folding: true,
-    minimap: {
-      enabled: false,
-    },
-    autoClosingBrackets: 'always',
-    autoClosingQuotes: 'always',
-    autoSurround: 'languageDefined',
-    bracketPairColorization: { enabled: true },
-  };
-
-  const copyToClipboard = async () => {
-    if (inputValue) {
-      try {
-        await navigator.clipboard.writeText(inputValue);
-        console.log('Text copied to clipboard');
-      } catch (err) {
-        console.error('Failed to copy: ', err);
-      }
-    }
+  const handleChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setValue(event.target.value);
   };
 
   return (
-    <Grid container item xs={6} height={'90%'} flexDirection={'column'}>
-      <Grid container height={'75%'}>
-        <Grid width={'93%'}>
-          <MonacoEditor
-            data-testid="monaco-editor"
-            height="100%"
-            theme="vs-dark"
-            language="graphql"
-            value={inputValue}
-            onChange={handleEditorChange}
-            options={options}
-          />
-        </Grid>
-        <ButtonGrid container>
-          <ButtonStart data-testid="play-button">
-            <PlayArrowIcon />
-          </ButtonStart>
-          <StyledButton onClick={copyToClipboard} data-testid="copy-button">
-            <ContentCopyIcon />
-          </StyledButton>
-        </ButtonGrid>
-      </Grid>
-      <GridTabs container>
-        <Grid sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <StyledTab label="Variables" {...a11yProps(0)} />
-            <StyledTab label="Headers" {...a11yProps(1)} />
-          </Tabs>
-        </Grid>
-        <CustomTabPanel value={value} index={0}>
-          <StyledTextField multiline variant="filled" />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          <StyledTextField multiline variant="filled" />
-        </CustomTabPanel>
-      </GridTabs>
-    </Grid>
+    <Wrapper container item xs={6}>
+      <StyledInput value={value} onChange={handleChange} />
+      <ButtonStart onClick={() => console.log(value)}>
+        <PlayArrowIcon />
+      </ButtonStart>
+    </Wrapper>
   );
 };
 
--- src/components/atoms/InputEntryPoint/style.ts
+++ src/components/atoms/InputEntryPoint/style.ts
@@ -1,70 +1,31 @@
-import { Button, Grid, Tab, TextField } from '@mui/material';
+import { Button, Grid, Input } from '@mui/material';
 import { styled } from '@mui/system';
 
-export const StyledTextField = styled(TextField)({
-  height: '100%',
-  width: '100%',
-  '.MuiInputBase-root': {
-    height: '100%',
-  },
-  textarea: {
-    color: '#fff',
-    height: '100% !important',
-  },
+export const Wrapper = styled(Grid)({
+  height: '5%',
+  backgroundColor: 'rgba(0, 0, 0, 0.09)',
+  borderTopRightRadius: '5px',
+  borderBottomRightRadius: '5px',
 });
 
-export const ButtonGrid = styled(Grid)({
-  width: '7%',
-  background: 'rgba(0, 0, 0, 0.06)',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  flexDirection: 'column',
-  paddingTop: '15px',
-  gap: '15px',
+export const StyledInput = styled(Input)({
+  width: '95%',
+  color: '#fff',
 });
 
 export const ButtonStart = styled(Button)({
   all: 'unset',
-  width: '40px',
-  height: '40px',
-  background: '#FF5794',
+  width: '5%',
+  height: '100%',
+  background: 'rgba(0, 0, 0, 0.09)',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  borderRadius: '8px',
-  cursor: 'pointer',
-  border: '1px solid #FF5794',
-  transition: 'all 0.5s',
-
-  span: {
-    display: 'none',
-  },
-});
-
-export const StyledButton = styled(Button)({
-  all: 'unset',
-  width: '42px',
-  height: '42px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderRadius: '8px',
+  borderRadius: '5px',
   cursor: 'pointer',
   transition: 'all 0.5s',
 
   span: {
     display: 'none',
   },
-});
-
-export const GridTabs = styled(Grid)({
-  flexDirection: 'column',
-  backgroundColor: 'rgba(0, 0, 0, 0.09)',
-  borderBottomLeftRadius: '4px',
-  borderBottomRightRadius: '4px',
-  height: '25%',
-});
-
-export const StyledTab = styled(Tab)({
-  color: '#fff',
 });
