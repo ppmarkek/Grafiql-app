eaf95ee8b554554a70e7cf9619a56fba0c64a0fe
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2024-01-06 13:53:31 +0200

    feat: add Doc on the welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-//GRAP-52 -->
-// import Header from './components/templates/Header/Header';
-// import InputEntryPoint from './components/atoms/InputEntryPoint/InputEntryPoint';
 import { ValueContext, Langs } from './components/Context/ValueContext';
 import WelcomePage from './pages/WelcomePage';
 import MainPage from './pages/mainPage/MainPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import SignUp from './pages/SignUp/SignUp';
 import SignIn from './pages/SignIn/SignIn';
-import Header from './components/templates/Header/Header';
 
 function App() {
   const [language, setLanguage] = useState<Langs>(Langs.en);
@@ -21,7 +16,6 @@ function App() {
       value={{ language, setLanguage, inputEntryPoint, setInputEntryPoint }}
     >
       <Router>
-        <Header />
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/main" element={<MainPage />} />
