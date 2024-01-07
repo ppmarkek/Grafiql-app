0f1c4f49c8bd4c70fbe29b2c00b339d977d77239
LisaW <LisaW@ya.ru>
2024-01-07 03:33:44 +0300

    fix: Header tests

--- src/components/templates/Header/tests/Header.spec.tsx
+++ src/components/templates/Header/tests/Header.spec.tsx
@@ -1,9 +1,36 @@
 import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
+import * as FirebaseModule from '../../../../services/auth/firebase';
+import * as useI18nModule from '../../../../components/Context/ValueContext';
+import lang from '../../../../i18n/en.json';
 import Header from '../Header';
 
+type useFirebaseAuthREturn = ReturnType<typeof FirebaseModule.useFirebaseAuth>;
+const mockedReturnedValue: useFirebaseAuthREturn = {
+  user: undefined,
+  error: undefined,
+  loading: false,
+  registerWithEmailAndPassword: jest.fn(),
+  logInWithEmailAndPassword: jest.fn(),
+  logout: jest.fn(),
+  signInWithGoogle: jest.fn(),
+};
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+  Link: () => <a></a>,
+}));
+
 describe('Header component', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(FirebaseModule, 'useFirebaseAuth')
+      .mockReturnValue(mockedReturnedValue);
+    jest.spyOn(useI18nModule, 'useI18n').mockReturnValue(lang);
+  });
   it('renders correctly and matches snapshot', () => {
     const { asFragment } = render(
       <MemoryRouter>
