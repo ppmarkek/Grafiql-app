c204eaf5381e87393bb901bb1cdb3a4e329bebe1
LisaW <LisaW@ya.ru>
2024-01-07 16:58:52 +0300

    fix: json

--- src/pages/SignIn/SignIn.test.tsx
+++ src/pages/SignIn/SignIn.test.tsx
@@ -1,9 +1,12 @@
+import '@testing-library/jest-dom';
 import React from 'react';
 import SignIn from './SignIn';
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import * as FirebaseModule from '../../services/auth/firebase';
 import * as useI18nModule from '../../components/Context/ValueContext';
 import lang from '../../i18n/en.json';
+import userEvent from '@testing-library/user-event';
 
 type useFirebaseAuthREturn = ReturnType<typeof FirebaseModule.useFirebaseAuth>;
 const mockedReturnedValue: useFirebaseAuthREturn = {
@@ -33,5 +36,16 @@ describe('SignIn', () => {
   it('should render without errors', () => {
     const tree = renderer.create(<SignIn />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('login not called', () => {
+    render(<SignIn />);
+    const btn = screen.getByText(lang.auth.buttonName);
+    expect(btn).toBeInTheDocument();
+    userEvent.click(btn);
+    expect(
+      mockedReturnedValue.logInWithEmailAndPassword
+    ).not.toHaveBeenCalled();
+    const gbtn = screen.getByText(lang.auth.googleButton);
+    expect(gbtn).toBeInTheDocument();
   });
 });
